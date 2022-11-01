c96382f1eb67d5ce0fc9cfbc7b815016b2c6c1d7
unknown <souhardya.dutta7801@gmail.com>
2022-11-01 22:09:01 +0530

    gopal gobinda

--- frictionServer.js
+++ frictionServer.js
@@ -38,6 +38,9 @@ const PORT = process.env.PORT || 5000;
 
 if(process.env.NODE_ENV === "production"){
     app.use(express.static("client/build"));
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
 }
 
 app.listen(PORT, console.log("server running in port "+ PORT));
