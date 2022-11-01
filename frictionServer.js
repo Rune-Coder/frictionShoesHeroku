fdb996b35dae546efc0235b6bc16a35ebc9c2f4c
unknown <souhardya.dutta7801@gmail.com>
2022-11-01 10:31:54 +0530

    make it better

---
+++ frictionServer.js
@@ -0,0 +1,42 @@
+import express from "express";
+import dotenv from "dotenv";
+import connectDatabase from "./config/MongoDb.js";
+import importData from "./dataImport.js";
+import productRoute from "./routers/productRouter.js";
+import { errorHandler, notFound } from "./middleware/error.js";
+import userRoute from "./routers/userRouter.js";
+import paymentRoute from "./routers/paymentRouter.js";
+
+
+dotenv.config();
+connectDatabase();
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+
+//api
+app.use("/api/import", importData);
+app.use("/api/products", productRoute);
+app.use("/api/user", userRoute);
+app.use("/api/payment", paymentRoute);
+
+//error handler
+app.use(notFound);
+app.use(errorHandler);
+
+app.get("/", (req, res)=> {
+    res.send("API is running...");
+});
+
+const PORT = process.env.PORT || 5000;
+
+//step 3 heroku
+
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static("client/build"));
+}
+
+app.listen(PORT, console.log("server running in port "+ PORT));
