59ade8e9e829e264ea885baa9df3e1447e179285
unknown <souhardya.dutta7801@gmail.com>
2022-11-01 12:44:47 +0530

    make it better

--- frictionServer.js
+++ frictionServer.js
@@ -6,6 +6,7 @@ import productRoute from "./routers/productRouter.js";
 import { errorHandler, notFound } from "./middleware/error.js";
 import userRoute from "./routers/userRouter.js";
 import paymentRoute from "./routers/paymentRouter.js";
+import path from "path";
 
 
 dotenv.config();
@@ -37,6 +38,9 @@ const PORT = process.env.PORT || 5000;
 
 if(process.env.NODE_ENV === "production"){
     app.use(express.static("client/build"));
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
 }
 
 app.listen(PORT, console.log("server running in port "+ PORT));
