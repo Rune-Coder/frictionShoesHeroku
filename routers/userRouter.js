66df081f8d035b6bdd5393a5aebf5f3e9f1b713e
unknown <souhardya.dutta7801@gmail.com>
2022-11-05 22:30:01 +0530

    mark 13

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const asyncHandler = require('express-async-handler');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const User = require('../models/userModel.js');
 const generateToken = require('../util/generateToken.js');
 const protect = require('../middleware/auth.js');
